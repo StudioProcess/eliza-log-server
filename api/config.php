8d789322b8a6115736f4b70019256ce6d37f2d39
Martin Grödl <martin@process.studio>
2021-11-12 11:10:24 +0100

    document config

--- api/config.php
+++ api/config.php
@@ -1,16 +1,16 @@
 <?php
 
 $DEBUG = true;
-$DEBUG_LOG = false;
-$LOGDIR = '../logs_test'; # relative to this file
+$DEBUG_LOG = false; # debug messages (i.e. request info) in log files
+$LOGDIR = '../logs'; # relative to this file
 $FILES = array(
-  'jwt-secret'    => './jwt.secret',
-  'git-sha'       => './git-sha',
-  'composer-json' => './composer.json'
+  'jwt-secret'    => './jwt.secret', # secret for JWT session tokens
+  'git-sha'       => './git-sha', # git sha (for /info)
+  'composer-json' => './composer.json' # contains name and version (for /info)
 );
 $EXPIRATION_SECS = 3600 * 24; # session expiration in seconds (use <=0 for no expiration)
-$CORS_ALLOW_ORIGINS = array('https://kg.laurentlang.com', 'https://sketch.process.studio', '*');
-$ALLOW_HOSTS = array('*');
+$CORS_ALLOW_ORIGINS = array('https://kg.laurentlang.com', 'https://sketch.process.studio', 'https://islandrabe.com'); # for Access-Control-Allow-Origin header
+$ALLOW_HOSTS = array('*'); # check $_SERVER['HTTP_HOST'] and $_SERVER["REQUEST_SCHEME"]
 $LOG_MESSAGE_MAX_LENGTH = 2048; # includes timestamp(s)
 
 ?>
