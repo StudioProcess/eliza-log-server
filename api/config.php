3943663033200a87bc8a7f004b786ebb8c614abc
Martin Grödl <martin@process.studio>
2023-02-07 09:39:12 +0000

    Update origins

--- api/config.php
+++ api/config.php
@@ -9,7 +9,7 @@ $FILES = array(
   'composer-json' => './composer.json' # contains name and version (for /info)
 );
 $EXPIRATION_SECS = 3600 * 24; # session expiration in seconds (use <=0 for no expiration)
-$CORS_ALLOW_ORIGINS = array('https://kg.laurentlang.com', 'https://sketch.process.studio', 'https://islandrabe.com'); # for Access-Control-Allow-Origin header
+$CORS_ALLOW_ORIGINS = array('https://kg.laurentlang.com', 'https://sketch.process.studio', 'https://islandrabe.com', 'https://knigge.chat'); # for Access-Control-Allow-Origin header
 $ALLOW_HOSTS = array('*'); # check $_SERVER['HTTP_HOST'] and $_SERVER["REQUEST_SCHEME"]
 $LOG_MESSAGE_MAX_LENGTH = 2048; # includes timestamp(s)
 
